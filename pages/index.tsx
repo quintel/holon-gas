0f8b27447249db0eafc47bb6b6a3759b221e9603
Anthony Williams <hi@antw.dev>
2022-05-09 17:25:33 +0100

    Switch to English

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,42 +11,42 @@ const Home: NextPage = () => {
         <div className="bg-gray-200 text-gray-600 rounded p-4 mb-6 h-40 flex items-center justify-center">
           Presets
         </div>
-        <FieldGroup title="Gasproductie" className="mb-6">
+        <FieldGroup title="Gas production" className="mb-6">
           <Input
-            name="Extra gas uit Groningen"
+            name="Extra gas from Groningen"
             min={0}
             max={10}
             defaultValue={5}
           />
           <Input
-            name="Extra gas uit andere EU landen"
+            name="Extra gas from other EU countries"
             min={0}
             max={50}
             defaultValue={0}
           />
         </FieldGroup>
-        <FieldGroup title="Overig productie">
+        <FieldGroup title="Other production">
           <Input
-            name="Her-inzet kolencentrales"
+            name="Re-use of coal-fired power stations"
             min={0}
             max={25}
             defaultValue={0}
           />
           <Input
-            name="Injectie biomethaan in gasmix"
+            name="Injection of biomethane in gas mix"
             min={0}
             max={20}
             defaultValue={10}
           />
           <Input
-            name="Groene waterstof als alternatie"
+            name="Green hydrogen as an alternative"
             min={3}
             max={6}
             defaultValue={4}
           />
         </FieldGroup>
         <div className="mt-3 px-5 text-center text-xs">
-          Meer opties bekijken? Ga naar de volledige versie van het{" "}
+          Want more options? Go to the full version of the{" "}
           <a href="https://pro.energytransitionmodel.com/">
             Energy Transition Model
           </a>
@@ -54,61 +54,61 @@ const Home: NextPage = () => {
         </div>
       </div>
       <div className="w-1/3 p-6">
-        <FieldGroup title="Besparing thuis en kantoor" className="mb-6">
-          <Input name="Isoleren panden" min={0} max={100} defaultValue={0} />
+        <FieldGroup title="Savings at home and office" className="mb-6">
+          <Input name="Insulate buildings" min={0} max={100} defaultValue={0} />
           <Input
-            name="Optimaal instellen boilers"
+            name="Optimal boiler settings"
             min={0}
             max={100}
             defaultValue={0}
           />
           <Input
-            name="Aanschaf zonnecollectoren"
+            name="Purchase solar collectors"
             min={0}
             max={100}
             defaultValue={0}
           />
           <Input
-            name="Percentage bevolking"
+            name="Population percentage"
             min={0}
             max={100}
             defaultValue={0}
           />
           <Input
-            name="Aantal graden lager"
+            name="Number of degrees lower"
             min={0}
             max={100}
             defaultValue={0}
           />
           <Input
-            name="Overstap warmtepomp"
+            name="Switch to heat pumps"
             min={0}
             max={100}
             defaultValue={0}
           />
-          <Input name="PV op daken" min={0} max={100} defaultValue={0} />
+          <Input name="PV on roofs" min={0} max={100} defaultValue={0} />
           <Input
-            name="Electriciteitsopslag achter de meter"
+            name="Electricity storage behind the meter"
             min={0}
             max={100}
             defaultValue={0}
           />
         </FieldGroup>
-        <FieldGroup title="Overig" className="mb-6">
+        <FieldGroup title="Other" className="mb-6">
           <Input
-            name="Grootschalige opslag"
+            name="Large-scale storage"
             min={0}
             max={100}
             defaultValue={0}
           />
           <Input
-            name="Sluiting industrie met gas als grondstof"
+            name="Closure of industry with gas as raw material"
             min={0}
             max={100}
             defaultValue={0}
           />
           <Input
-            name="Industrie: bio-methaan als grondstof"
+            name="Industry: bio-methane as raw material"
             min={0}
             max={100}
             defaultValue={0}
@@ -116,21 +116,21 @@ const Home: NextPage = () => {
         </FieldGroup>
       </div>
       <div className="w-1/3 p-6">
-        <h2 className="text-lg">Resultaten</h2>
+        <h2 className="text-lg">Results</h2>
         <div className="bg-gray-200 text-gray-600 rounded p-4 mb-6 h-48 flex items-center justify-center">
           Chart
         </div>
 
-        <h2 className="text-lg mt-3">Geld naar Putin</h2>
-        <p>Het gel dat (alleen voor gas) dit jaar alsnog naar Rusland gaat.</p>
+        <h2 className="text-lg mt-3">Money to Putin</h2>
+        <p>Payments to Russia for gas this year</p>
         <div className="h-6 bg-gray-200 rounded mt-1" />
 
-        <h2 className="text-lg mt-3">Kosten</h2>
-        <p>Benodigde (eenmalige) investering</p>
+        <h2 className="text-lg mt-3">Costs</h2>
+        <p>Required (one-off) investment</p>
         <div className="h-6 bg-gray-200 rounded mt-1" />
 
-        <h2 className="text-lg mt-3">Utistoot</h2>
-        <p>Effect op EU uitstoot-cijfers</p>
+        <h2 className="text-lg mt-3">Emissions</h2>
+        <p>Effect on EU emissions figures</p>
         <div className="h-6 bg-gray-200 rounded mt-1" />
       </div>
     </div>
