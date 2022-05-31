73e9e0edbd051338db9da329697c0a87abac59d1
Anthony Williams <hi@antw.dev>
2022-05-31 19:14:47 +0100

    Make the insulation toggles look more like buttons

--- features/scenario/InsulationInput.tsx
+++ features/scenario/InsulationInput.tsx
@@ -4,20 +4,25 @@ import { RadioGroup } from "@headlessui/react";
 import { useAppSelector, useAppDispatch } from "../hooks";
 import { setInputValue, createInputSelector, InputKey } from "./scenario-slice";
 
-const baseItemClasses = "text-center bg-gray-200 py-0.5 cursor-pointer rounded";
-const checkedItemClasses = `${baseItemClasses} bg-blue-500 text-white border-blue-500`;
+const baseItemClasses =
+  "text-center bg-gray-200 hover:bg-gray-300 transition py-0.5 cursor-pointer";
+const checkedItemClasses = `${baseItemClasses} bg-blue-500 hover:bg-blue-500 text-white border-blue-500`;
 
 function Item({
   children,
+  className,
   value,
 }: {
   children: React.ReactNode;
+  className?: string;
   value: number;
 }): React.ReactElement {
   return (
     <RadioGroup.Option value={value} className="w-1/3">
       {({ checked }) => (
-        <div className={checked ? checkedItemClasses : baseItemClasses}>{children}</div>
+        <div className={`${className} ${checked ? checkedItemClasses : baseItemClasses}`}>
+          {children}
+        </div>
       )}
     </RadioGroup.Option>
   );
@@ -35,10 +40,14 @@ export default function InsulationInput({ id }: { id: InputKey }): React.ReactEl
   return (
     <div className="pb-4">
       <p className="pb-2">{input.name}</p>
-      <RadioGroup value={input.value} onChange={onChange} className="flex align-middle gap-2">
-        <Item value={0}>None</Item>
+      <RadioGroup value={input.value} onChange={onChange} className="flex align-middle">
+        <Item value={0} className="rounded-l mr-px">
+          None
+        </Item>
         <Item value={1}>Linear</Item>
-        <Item value={2}>Exponential</Item>
+        <Item value={2} className="rounded-r ml-px">
+          Exponential
+        </Item>
       </RadioGroup>
     </div>
   );
