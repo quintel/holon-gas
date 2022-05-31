dc12b278c8e5294a7f0850b0fe882709d39bfa52
Anthony Williams <hi@antw.dev>
2022-05-31 16:11:33 +0100

    Switch to two columns on narrow displays

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,57 +34,62 @@ const Home: NextPage = () => {
 
   return (
     <div className="container mx-auto flex text-sm">
-      <div className="w-1/3 p-6 pt-12">
-        <PresetSelection />
-        <FieldGroup title="Gas production" className="mb-6">
-          <Input id="extra_gas_from_groningen" />
-          <Input id="extra_gas_from_eu" />
-        </FieldGroup>
-        <FieldGroup title="Other production">
-          <FieldSubGroup title="Coal power plant capacity">
-            <Input id="coal_power_plant_capacity_conventional" />
-            <Input id="coal_power_plant_capacity_lignite" />
-          </FieldSubGroup>
-          <Input id="injection_of_biomethane_in_gas_mix" />
-          <Input id="lng_imports" />
-          <Input id="green_hydrogen" />
-          <Input id="renewable_energy_capacity" />
-        </FieldGroup>
-        <div className="mt-3 px-5 text-center text-xs">
-          Want more options? Go to the full version of the{" "}
-          <a href="https://pro.energytransitionmodel.com/">Energy Transition Model</a>.
+      <div className="w-full xl:w-2/3 pt-6 flex flex-wrap">
+        <div className="w-full xl:w-1/2 p-6">
+          <PresetSelection />
+          <FieldGroup title="Gas production" className="mb-6">
+            <Input id="extra_gas_from_groningen" />
+            <Input id="extra_gas_from_eu" />
+          </FieldGroup>
+          <FieldGroup title="Other production">
+            <FieldSubGroup title="Coal power plant capacity">
+              <Input id="coal_power_plant_capacity_conventional" />
+              <Input id="coal_power_plant_capacity_lignite" />
+            </FieldSubGroup>
+            <Input id="injection_of_biomethane_in_gas_mix" />
+            <Input id="lng_imports" />
+            <Input id="green_hydrogen" />
+            <Input id="renewable_energy_capacity" />
+          </FieldGroup>
+          <div className="mt-6 px-5 text-center text-xs leading-5">
+            Want more options? Go to the full version of the
+            <br />
+            <a href="https://pro.energytransitionmodel.com/" className="p-1">
+              Energy Transition Model
+            </a>
+          </div>
+        </div>
+        <div className="w-full xl:w-1/2 p-6">
+          <FieldGroup title="Savings at home and office" className="mb-6">
+            <Input id="electricity_storage_behind_the_meter" />
+            <Input id="growth_of_installed_heat_pumps" />
+            <InsulationInput id="insulation" />
+            <FieldSubGroup title="Thermostat settings">
+              <Input id="thermostat_settings_percentage" />
+              <Input id="thermostat_settings_reduce_temperature" />
+            </FieldSubGroup>
+            <FieldSubGroup title="Behavioural changes">
+              <Input id="behavioural_change_percentage" />
+            </FieldSubGroup>
+          </FieldGroup>
+          <FieldGroup title="Other" className="mb-6">
+            <Input id="solar_thermal_collectors" />
+            <FieldSubGroup title="Rooftop solar">
+              <Input id="rooftop_pv_households" />
+              <Input id="rooftop_pv_buildings" />
+            </FieldSubGroup>
+            <FieldSubGroup title="Large-scale energy storage">
+              <Input id="large_scale_storage_batteries" />
+              <Input id="large_scale_storage_reservoirs" />
+            </FieldSubGroup>
+            <FieldSubGroup title="Gas use in industry">
+              <Input id="replacement_of_gas_by_oil_in_chemical_industry" />
+              <Input id="closure_of_fertiliser_industry" />
+            </FieldSubGroup>
+          </FieldGroup>
         </div>
       </div>
-      <div className="w-1/3 p-6 pt-12">
-        <FieldGroup title="Savings at home and office" className="mb-6">
-          <Input id="electricity_storage_behind_the_meter" />
-          <Input id="growth_of_installed_heat_pumps" />
-          <InsulationInput id="insulation" />
-          <FieldSubGroup title="Thermostat settings">
-            <Input id="thermostat_settings_percentage" />
-            <Input id="thermostat_settings_reduce_temperature" />
-          </FieldSubGroup>
-          <FieldSubGroup title="Behavioural changes">
-            <Input id="behavioural_change_percentage" />
-          </FieldSubGroup>
-        </FieldGroup>
-        <FieldGroup title="Other" className="mb-6">
-          <Input id="solar_thermal_collectors" />
-          <FieldSubGroup title="Rooftop solar">
-            <Input id="rooftop_pv_households" />
-            <Input id="rooftop_pv_buildings" />
-          </FieldSubGroup>
-          <FieldSubGroup title="Large-scale energy storage">
-            <Input id="large_scale_storage_batteries" />
-            <Input id="large_scale_storage_reservoirs" />
-          </FieldSubGroup>
-          <FieldSubGroup title="Gas use in industry">
-            <Input id="replacement_of_gas_by_oil_in_chemical_industry" />
-            <Input id="closure_of_fertiliser_industry" />
-          </FieldSubGroup>
-        </FieldGroup>
-      </div>
-      <div className="w-1/3 p-6 pt-12 sticky top-0 self-start">
+      <div className="w-full xl:w-1/3 p-6 pt-12 sticky top-0 self-start">
         <h2 className="text-lg">Results</h2>
         <ResultsChart />
         <ColumnResults />
