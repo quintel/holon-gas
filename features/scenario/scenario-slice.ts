e6111caf3bbfbb288499306de9305844b4cf5665
Anthony Williams <hi@antw.dev>
2022-06-20 15:53:46 +0100

    Set gas price to 1.33

--- features/scenario/scenario-slice.ts
+++ features/scenario/scenario-slice.ts
@@ -133,7 +133,7 @@ const sendRequest = async (
     body: JSON.stringify({
       autobalance: true,
       gqueries: queries,
-      scenario: { user_values: inputs },
+      scenario: { user_values: { ...inputs, costs_gas: 1.33 } },
     }),
     signal,
   });
