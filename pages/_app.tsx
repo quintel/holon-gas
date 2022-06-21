60d553b827c444bd7d3673460e7855f9ec806371
Anthony Williams <hi@antw.dev>
2022-06-21 17:12:03 +0100

    Bump react-joyride to 2.5 and re-enable Strict Mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -8,9 +10,11 @@ import { store } from "../features/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <React.StrictMode>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </React.StrictMode>
   );
 }
 
