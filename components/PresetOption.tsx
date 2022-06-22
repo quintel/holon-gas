1e0928c35b20a8f88b613725f9857ad39d794c9e
Anthony Williams <hi@antw.dev>
2022-06-22 16:48:06 +0100

    Use ticks to indicate which preset is selected

--- components/PresetOption.tsx
+++ components/PresetOption.tsx
@@ -1,9 +1,39 @@
 import { RadioGroup } from "@headlessui/react";
 
-const commonClasses =
-  "py-1 border px-3 bg-gradient-to-b rounded cursor-pointer font-medium mb-2 transition";
-const baseClasses = `${commonClasses} border-white bg-white text-gray-900`;
-const checkedClasses = `${commonClasses} border-blue-500 bg-blue-500 from-white/30 text-white`;
+// const commonClasses = "py-1 px-3 bg-gradient-to-b rounded cursor-pointer font-medium transition";
+// const baseClasses = `${commonClasses} bg-white text-gray-900`;
+// const checkedClasses = `${commonClasses} bg-blue-500 from-white/30 text-white`;
+const commonClasses = "cursor-pointer font-medium";
+const baseClasses = `${commonClasses} text-gray-800`;
+const checkedClasses = `${commonClasses} text-green-700`;
+
+function Pill({ checked }: { checked: boolean }) {
+  const borderWidth = checked ? "10px" : "2px";
+  const colorClasses = checked
+    ? "text-green-600"
+    : "text-gray-400 group-hover:text-gray-500 transition-colors";
+
+  return (
+    <div
+      className={`mr-2 flex h-5 w-5 items-center justify-center rounded-full ${colorClasses}`}
+      style={{ boxShadow: `inset 0 0 0 ${borderWidth} currentColor` }}
+    >
+      <svg
+        viewBox="0 0 12 10"
+        className="h-3 w-3 text-gray-200"
+        style={{
+          fill: "none",
+          strokeWidth: 2,
+          stroke: "currentcolor",
+          strokeDasharray: 16,
+          strokeDashoffset: 0,
+        }}
+      >
+        <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+      </svg>
+    </div>
+  );
+}
 
 interface Props {
   title: string;
@@ -14,9 +44,15 @@ export default function PresetOptions({ title, value }: Props): React.ReactEleme
   return (
     <RadioGroup.Option
       value={value}
-      className="rounded outline-none ring-blue-300 ring-offset-2 ring-offset-gray-200 focus-visible:ring"
+      className="group mb-2 rounded outline-none ring-blue-300 ring-offset-2 ring-offset-gray-200 focus-visible:ring"
     >
-      {({ checked }) => <div className={checked ? checkedClasses : baseClasses}>{title}</div>}
+      {({ checked }) => (
+        <div className="flex items-center">
+          <Pill checked={checked} />
+          {/* {checked ? <CheckedPill /> : <UncheckedPill />} */}
+          <div className={checked ? checkedClasses : baseClasses}>{title}</div>
+        </div>
+      )}
     </RadioGroup.Option>
   );
 }
