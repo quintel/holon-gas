9655d0c1e5ad20441b77236610395a18cdea59c2
Anthony Williams <hi@antw.dev>
2022-06-22 16:48:35 +0100

    Use a pill style for insulation options

--- features/scenario/InsulationInput.tsx
+++ features/scenario/InsulationInput.tsx
@@ -7,7 +7,7 @@ import { setInputValue, createInputSelector, InputKey } from "./scenario-slice";
 import InputDescription from "../../components/InputDescription";
 import InputTitle from "../../components/InputTitle";
 
-const baseItemClasses = "bg-gray-200 border-gray-200 hover:border-gray-300 hover:bg-gray-300";
+const baseItemClasses = "bg-gray-200 border-gray-200 hover:border-gray-100 hover:bg-gray-100";
 const checkedItemClasses = "bg-blue-500 from-white/30 hover:bg-blue-500 text-white border-blue-500";
 
 function Item({
@@ -22,13 +22,13 @@ function Item({
   return (
     <RadioGroup.Option
       value={value}
-      className="relative w-1/3 outline-none first:mr-px first:rounded-l last:ml-px last:rounded-r focus-visible:z-10 focus-visible:ring-2 focus-visible:ring-offset-2"
+      className="relative w-1/3 rounded outline-none first:mr-px last:ml-px focus-visible:z-10 focus-visible:ring-2 focus-visible:ring-offset-2"
     >
       {({ checked }) => (
         <div
           className={`${className} ${
             checked ? checkedItemClasses : baseItemClasses
-          } cursor-pointer border bg-gradient-to-b py-0.5 text-center outline-none transition`}
+          } cursor-pointer rounded bg-gradient-to-b py-0.5 text-center outline-none transition`}
         >
           {children}
         </div>
@@ -50,14 +50,14 @@ export default function InsulationInput({ id }: { id: InputKey }): React.ReactEl
     <div className="pb-4">
       <InputTitle helpText={input.helpText}>{input.name}</InputTitle>
       {input.description && <InputDescription>{input.description}</InputDescription>}
-      <RadioGroup value={input.value} onChange={onChange} className="mt-1 flex align-middle">
-        <Item value={0} className="rounded-l">
-          None
-        </Item>
+      <RadioGroup
+        value={input.value}
+        onChange={onChange}
+        className="mt-1 flex gap-1 rounded border-4 bg-gray-200 align-middle"
+      >
+        <Item value={0}>None</Item>
         <Item value={1}>Linear</Item>
-        <Item value={2} className="rounded-r">
-          Exponential
-        </Item>
+        <Item value={2}>Exponential</Item>
       </RadioGroup>
     </div>
   );
