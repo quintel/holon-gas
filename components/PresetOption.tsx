f31cc1e45227d32c48247a08a2e2f51b91dc093c
Anthony Williams <hi@antw.dev>
2022-06-22 17:14:56 +0100

    Further adjust the unchecked preset option styles

--- components/PresetOption.tsx
+++ components/PresetOption.tsx
@@ -5,29 +5,34 @@ const baseClasses = `${commonClasses} text-gray-800`;
 const checkedClasses = `${commonClasses} text-green-700`;
 
 function Pill({ checked }: { checked: boolean }) {
-  const borderWidth = checked ? "10px" : "2px";
+  const borderWidth = checked ? "10px" : "3.3px";
   const colorClasses = checked
     ? "text-green-600"
-    : "text-gray-400 group-hover:text-gray-500 transition-colors";
+    : "text-gray-400 bg-gray-50 group-hover:text-gray-600 transition-all scale-75";
 
   return (
     <div
-      className={`mr-2 flex h-5 w-5 items-center justify-center rounded-full ${colorClasses}`}
-      style={{ boxShadow: `inset 0 0 0 ${borderWidth} currentColor` }}
+      className={`mr-2 flex h-5 w-5 items-center justify-center rounded-full transition ${colorClasses}`}
+      style={{
+        boxShadow: `inset 0 0 0 ${borderWidth} currentColor`,
+        transform: `scale(${checked ? "1.0" : "0.61"})`,
+      }}
     >
-      <svg
-        viewBox="0 0 12 10"
-        className="h-3 w-3 text-gray-200"
-        style={{
-          fill: "none",
-          strokeWidth: 2,
-          stroke: "currentcolor",
-          strokeDasharray: 16,
-          strokeDashoffset: 0,
-        }}
-      >
-        <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-      </svg>
+      {checked && (
+        <svg
+          viewBox="0 0 12 10"
+          className="h-3 w-3 text-gray-200"
+          style={{
+            fill: "none",
+            strokeWidth: 2,
+            stroke: "currentcolor",
+            strokeDasharray: 16,
+            strokeDashoffset: 0,
+          }}
+        >
+          <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+        </svg>
+      )}
     </div>
   );
 }
