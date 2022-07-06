64e72c770fcea7735d3941c74e27425a2cf984d8
Anthony Williams <hi@antw.dev>
2022-07-06 14:02:49 +0100

    Swap out the tour icon for a map
    This aligns better with the button text and better matches the button
    than the light bulb.

--- components/ToolHeader.tsx
+++ components/ToolHeader.tsx
@@ -24,7 +24,7 @@ const ToolHeader: React.FC<{}> = () => {
                 strokeLinejoin="round"
                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
-            </svg>{" "}
+            </svg>
             About the tool
           </a>
         </Link>
--- features/tour/Tour.tsx
+++ features/tour/Tour.tsx
@@ -64,7 +64,7 @@ const Tour: React.FC<{}> = () => {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="mr-1 -mt-0.5 mb-0.5 h-6 w-6"
+          className="mr-1 h-6 w-6"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -73,9 +73,9 @@ const Tour: React.FC<{}> = () => {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+            d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"
           />
-        </svg>{" "}
+        </svg>
         Take a quick tour
       </button>
       <Joyride
