401ed03552398df4581e3d900be203b67e607e4d
Anthony Williams <hi@antw.dev>
2022-09-29 11:25:47 +0100

    Stub react-markdown to fix tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,10 @@
 import "@testing-library/jest-dom/extend-expect";
+
+// Stub out react-markdown which doesn't work in Jest due to Jest's support for ESM.
+// https://github.com/remarkjs/react-markdown/issues/635
+// eslint-disable-next-line react/display-name
+jest.mock("react-markdown", () => (props) => {
+  return <>{props.children}</>;
+});
+
+jest.mock("remark-gfm", () => undefined);
